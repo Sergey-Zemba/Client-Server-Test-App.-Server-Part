17f739b01b894977fcce152b617a072ebfb34376
Sergey-Zemba <sergey.zemba@gmail.com>
2016-06-07 15:06:04 +0300

    Server Part

---
+++ Server/ClientObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ClientObject
+    {
+        private TcpClient client;
+
+        public ClientObject(TcpClient client)
+        {
+            this.client = client;
+        }
+
+        public void Process()
+        {
+            NetworkStream stream = null;
+            try
+            {
+                stream = client.GetStream();
+                byte[] data = new byte[64];
+                while (true)
+                {
+                    StringBuilder messageBuilder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        messageBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (stream.DataAvailable);
+                    string message = messageBuilder.ToString();
+                    Console.WriteLine(message);
+                    message = message.Substring(message.IndexOf(':') + 1).Trim().ToUpper();
+                    data = Encoding.Unicode.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        private static int port = Int32.Parse(ConfigurationManager.AppSettings["port"]);
+        private static string address = ConfigurationManager.AppSettings["address"];
+        private static TcpListener listener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse(address), port);
+                listener.Start();
+                Console.WriteLine("Waiting for connections...");
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    ClientObject clientObject = new ClientObject(client);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+            }
+        }
+    }
+}
